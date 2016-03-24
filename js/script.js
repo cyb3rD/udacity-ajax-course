79954186cf5e67555db651ec3b5fc75d5670d2df
Nikolay Ignatyev <noilgt@gmail.com>
2016-03-24 23:30:17 +0300

    Added NYT Ajax request

--- js/script.js
+++ js/script.js
@@ -12,12 +12,30 @@ function loadData() {
     $nytElem.text("");
 
     // load streetview
-
-    // YOUR CODE GOES HERE!
-    var mapsGoogleSrc = 'https://maps.googleapis.com/maps/api/streetview?size=600x400&location='
+    var mapsGoogleSrc = 'https://maps.googleapis.com/maps/api/streetview?size=600x400&location=';
     var street = $('#street').val();
     var city = $('#city').val();
     $body.append('<img class="bgimg" src="' + mapsGoogleSrc + city + ', ' + street + '">');
+
+
+    var apiNYTKey = 'aa4cb30b32ca1932efccf22db70e420d:15:74798596';
+    var searchQuery =  city,
+        searchUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q='+ city + '&api-key='+ apiNYTKey ;
+
+    // usign NYT API
+    $.getJSON(searchUrl, function(json, textStatus) {
+            /*optional stuff to do after success */
+            console.log(json);
+            var articles = json.response.docs;
+
+            for (var i = 0; i < articles.length; i++) {
+                var paragraph = articles[i].lead_paragraph,
+                    headline = articles[i].headline.main,
+                    articleLink = articles[i].web_url;
+                $nytElem.append('<li><a href="' + articleLink + '">' + headline + '</a><p>'+ paragraph + '</p></li>');
+            }
+    });
+
     return false;
 };
 
